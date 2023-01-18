c41dbeb84531f9e7893eb85c139b8830ae97ab8f
KenStarry <starrycodes@gmail.com>
2023-01-18 23:52:27 +0300

    Added Sort radio buttons

--- app/src/main/java/com/example/houseopscaretakers/feature_houses/home_screen/presentation/components/bottom_sheet/SortBottomSheet.kt
+++ app/src/main/java/com/example/houseopscaretakers/feature_houses/home_screen/presentation/components/bottom_sheet/SortBottomSheet.kt
@@ -1,24 +1,37 @@
 package com.example.houseopscaretakers.feature_houses.home_screen.presentation.components.bottom_sheet
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.example.houseopscaretakers.feature_houses.home_screen.data.HomeConstants
 
 @Composable
-fun SortBottomSheet() {
+fun SortBottomSheet(
+    onOptionSelected: (option: String) -> Unit
+) {
+
+    var selectedOption by remember {
+        mutableStateOf(HomeConstants.sortOptions[0])
+    }
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(24.dp)
+            .padding(24.dp),
+        verticalArrangement = Arrangement.spacedBy(24.dp)
     ) {
 
         //  sort title
@@ -27,6 +40,52 @@ fun SortBottomSheet() {
             fontSize = MaterialTheme.typography.titleMedium.fontSize,
             fontWeight = FontWeight.Bold,
             color = MaterialTheme.colorScheme.onSecondaryContainer
+        )
+
+        LazyColumn(
+            content = {
+                items(
+                    items = HomeConstants.sortOptions
+                ) { option ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected = selectedOption == option,
+                                onClick = {
+                                    selectedOption = option
+                                    onOptionSelected(selectedOption)
+                                }
+                            ),
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        //  radio button
+                        RadioButton(
+                            selected = selectedOption == option,
+                            onClick = {
+                                selectedOption = option
+                                onOptionSelected(selectedOption)
+                            },
+                            colors = RadioButtonDefaults.colors(
+                                selectedColor = MaterialTheme.colorScheme.primary
+                            )
+                        )
+
+                        //  sort text
+                        Text(
+                            text = option,
+                            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                            fontWeight = MaterialTheme.typography.bodyMedium.fontWeight,
+                            color = MaterialTheme.colorScheme.onSecondaryContainer
+                        )
+
+                    }
+                }
+            },
+            state = rememberLazyListState(),
+            contentPadding = PaddingValues(vertical = 8.dp)
         )
 
 

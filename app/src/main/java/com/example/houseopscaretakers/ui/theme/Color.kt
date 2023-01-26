2516c2a0cb843e76af379a825796aaeb1d3f4e90
KenStarry <starrycodes@gmail.com>
2023-01-26 15:46:24 +0300

    Adjusted the Colors

--- app/src/main/java/com/example/houseopscaretakers/ui/theme/Color.kt
+++ app/src/main/java/com/example/houseopscaretakers/ui/theme/Color.kt
@@ -10,7 +10,7 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val GreyBackground = Color(0xFFEFF0F5)
+val GreyBackground = Color(0xFFF5F6FA)
 val WhiteBackground = Color(0xFFffffff)
 val DarkBackground = Color(0xFF292B2C)
 val BlackBackgroundAlt = Color(0xFF18191A)

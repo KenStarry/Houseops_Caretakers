d4d4513fd70794fe83d81130532d6ec86a10db79
KenStarry <starrycodes@gmail.com>
2023-02-25 16:35:00 +0300

    Pick location section

--- app/src/main/java/com/example/houseopscaretakers/feature_landlord/feature_home/feature_add_apartment/presentation/components/LndApartmentDetails.kt
+++ app/src/main/java/com/example/houseopscaretakers/feature_landlord/feature_home/feature_add_apartment/presentation/components/LndApartmentDetails.kt
@@ -1,10 +1,18 @@
 package com.example.houseopscaretakers.feature_landlord.feature_home.feature_add_apartment.presentation.components
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Apartment
+import androidx.compose.material.icons.outlined.ArrowRight
+import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,7 +24,8 @@ import com.example.houseopscaretakers.core.presentation.components.CustomTextFie
 
 @Composable
 fun LndApartmentDetails(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    onLocationClicked: () -> Unit
 ) {
 
     Column(
@@ -41,7 +50,112 @@ fun LndApartmentDetails(
             }
         )
 
-        //  apartment details
+        //  apartment details section
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+
+            //  title
+            Text(
+                text = "Apartment Details",
+                fontSize = MaterialTheme.typography.titleMedium.fontSize,
+                fontWeight = FontWeight.ExtraBold,
+                color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.7f)
+            )
+
+            //  section items
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .wrapContentHeight()
+                    .background(
+                        color = MaterialTheme.colorScheme.onSecondary,
+                        shape = RoundedCornerShape(16.dp)
+                    )
+                    .padding(8.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+
+                //  location
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .wrapContentHeight(),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+
+                    Text(
+                        text = "Pick Location",
+                        fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f)
+                    )
+
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowRight,
+                        contentDescription = "Right arrow",
+                        tint = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f)
+                    )
+
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .wrapContentHeight()
+                            .background(
+                                color = MaterialTheme.colorScheme.tertiary,
+                                shape = RoundedCornerShape(16.dp)
+                            )
+                            .clickable {
+                                //  open location bottomsheet
+                                onLocationClicked()
+                            }
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+
+                        Text(
+                            text = "No Location",
+                            fontSize = MaterialTheme.typography.bodyMedium.fontSize,
+                            fontWeight = FontWeight.Bold,
+                            color = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f)
+                        )
+
+                        Icon(
+                            imageVector = Icons.Outlined.ExpandMore,
+                            contentDescription = "expand arrow",
+                            tint = MaterialTheme.colorScheme.onSecondaryContainer.copy(alpha = 0.8f)
+                        )
+
+                    }
+
+                }
+
+                //  house categories
+                //  apartment features e.g security type, social ammenities... etc (list)
+                //  apartment terms and conditions (list)
+
+            }
+        }
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
